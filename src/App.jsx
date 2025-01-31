13b6108260e2af33aec5c4f84bc81d249943de9c
Siddharth Shah <sidshah895@gmail.com>
2025-01-31 23:47:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-green-400 text-center">
+        spotify project initial setup
+      </h1>
+    </>
+  );
+}
+
+export default App;
